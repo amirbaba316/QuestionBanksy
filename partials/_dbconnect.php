18e7b08fdce33d5e75200310edbead29908ad45b
amir <babaamir325@gmail.com>
2022-07-05 10:55:07 +0530

    Index page

---
+++ partials/_dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+   
+
+   $servername = "localhost";
+   $username   = "root";
+   $password   = "";
+   $database   = "QuestionBanksy_db";
+
+
+   $conn       = mysqli_connect($servername,$username,$password,$database);
+?>
---
+++ partials/_header.php
@@ -0,0 +1,36 @@
+<?php
+echo '
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#"><em><img src="qbanksy/img/q_logo.jpg" alt="...">QuestionBANKSY</em></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item dropdown active">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false">
+                    Categories
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">Action</a>
+                    <a class="dropdown-item" href="#">Another action</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active" href="#" tabindex="-1" aria-disabled="true">Contact</a>
+            </li>
+        </ul>
+    </div>
+</nav>';
+?>
